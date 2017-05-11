2f62719ccc3646bada07e53268fbeb38150a346e
David Cava Jimenez <davidcavajimenez@iMac-de-David.local>
2017-05-11 21:35:02 +0200

    Primer Test

---
+++ Money/Euro.swift
@@ -0,0 +1,14 @@
+//
+//  Euro.swift
+//  Money
+//
+//  Created by David Cava Jimenez on 11/5/17.
+//  Copyright © 2017 David Cava Jimenez. All rights reserved.
+//
+
+import Foundation
+
+
+class Euro{
+    
+}
--- MoneyTests/MoneyTests.swift
+++ MoneyTests/MoneyTests.swift
@@ -21,16 +21,11 @@ class MoneyTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCreateEuro() {
+        
+        XCTAssertNotNil(Euro())
+    
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
     
 }
